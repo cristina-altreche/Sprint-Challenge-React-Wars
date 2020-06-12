0184dd4eb3206d67c994ad3cf8cce5ad77cd2f90
Cristina Altreche <c.altreche@gmail.com>
2020-06-12 11:22:15 -0500

    characters show with name and images

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Character from "./components/Character.js";
+
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [rmData, setRMData] = useState([])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    const fetchData = () => {
+      axios
+        .get(
+          `https://rickandmortyapi.com/api/character/
+          `
+        )
+        .then((res) => {
+          setRMData(res.data.results)
+          console.log(res.data.results)
+        })
+        .catch((err) => {
+          console.log(err, "this did not work");
+        });
+    };
+    fetchData();
+  }, []);
+
+
+
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character charData={rmData}></Character>
+
+      <button>Next Char</button>
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,30 @@
 // Write your Character component here
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+} from "reactstrap";
+
+const Character = (props) => {
+  const { charData } = props;
+
+  return (
+    <div className="char-container">
+      {charData.map((characters) => {
+        return (
+          <Card>
+            <CardTitle>{characters.name}</CardTitle>
+            <CardImg src={characters.image}></CardImg>
+          </Card>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Character;
